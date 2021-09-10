79296fdfc5c107c7aeda4ec5509ced063a55421e
John Wass <jwass3@gmail.com>
2021-09-10 17:36:58 -0400

    add esri

--- app/src/main/java/com/github/jw3/harvest21/MainActivity.kt
+++ app/src/main/java/com/github/jw3/harvest21/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.github.jw3.harvest21
 
+import android.Manifest
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.preference.PreferenceManager
+import com.esri.arcgisruntime.geometry.SpatialReference
+import com.esri.arcgisruntime.mapping.ArcGISMap
 import kotlinx.android.synthetic.main.activity_main.*
 import org.eclipse.paho.android.service.MqttAndroidClient
 import org.eclipse.paho.client.mqttv3.IMqttActionListener
@@ -28,6 +34,12 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        askGpsPermission()
+
+        mapView.map = ArcGISMap(SpatialReference.create(26917))
+        mapView.locationDisplay.isShowLocation = true
+        mapView.locationDisplay.startAsync()
 
         val prefs = PreferenceManager.getDefaultSharedPreferences(this)
         val deviceId = prefs.getString("device_uid", "")
@@ -58,6 +70,24 @@ class MainActivity : AppCompatActivity() {
         } catch (e: Exception) {
             theText.text = e.message
             return
+        }
+    }
+
+    private fun askGpsPermission() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE)
+            + ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
+            + ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+            != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(
+                    Manifest.permission.ACCESS_COARSE_LOCATION,
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.READ_EXTERNAL_STORAGE
+                ),
+                111
+            )
         }
     }
 }
