5c5900dc221b81375083e1853ce194d1366e0299
John Wass <jwass3@gmail.com>
2021-09-18 21:31:12 -0400

    pref for using mqtt for self device location

--- app/src/main/java/com/github/jw3/harvest21/di/MapModule.kt
+++ app/src/main/java/com/github/jw3/harvest21/di/MapModule.kt
@@ -7,16 +7,18 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.FragmentComponent
+import dagger.hilt.android.components.ServiceComponent
 import dagger.hilt.android.qualifiers.ApplicationContext
 
 @Module
-@InstallIn(FragmentComponent::class)
+@InstallIn(FragmentComponent::class, ServiceComponent::class)
 class MapModule {
     @Provides
     fun providePrefs(@ApplicationContext applicationContext: Context): MapPrefs {
         val prefs = PreferenceManager.getDefaultSharedPreferences(applicationContext)
         return MapPrefs.newInstance(
-            prefs.getBoolean("map_auto_pan", false)
+            prefs.getBoolean("map_auto_pan", false),
+            prefs.getBoolean("map_echo_location", false)
         )
     }
 }
--- app/src/main/java/com/github/jw3/harvest21/prefs/MapPrefs.kt
+++ app/src/main/java/com/github/jw3/harvest21/prefs/MapPrefs.kt
@@ -2,12 +2,15 @@ package com.github.jw3.harvest21.prefs
 
 interface MapPrefs {
     val autoPan: Boolean
+    val echoLocation: Boolean
 
     companion object {
         @JvmStatic
-        fun newInstance(autoPan: Boolean) = object: MapPrefs {
+        fun newInstance(autoPan: Boolean, echoLocation: Boolean) = object: MapPrefs {
             override val autoPan: Boolean
                 get() = autoPan
+            override val echoLocation: Boolean
+                get() = echoLocation
         }
     }
 }
